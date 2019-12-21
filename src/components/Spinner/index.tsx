c90c5e04aaa3ccefc81f22d84f03a015f6bda917
Sergey <serg.lesn@gmail.com>
2019-12-21 16:27:06 +0200

    added spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Spinner as Loader } from 'reactstrap';
+
+
+const Spinner = () => {
+  return (
+    <div className="container mt-5 text-center">
+      <Loader />
+    </div>
+  );
+};
+
+export default Spinner;
